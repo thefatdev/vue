adad2227ce43e6790f5a827bc1cf4722cce10452
System Administrator <root@TRANs-MacBook-Air.local>
2022-05-14 11:54:01 +0700

    update vue

--- hocvue1/src/App.vue
+++ hocvue1/src/App.vue
@@ -1,17 +1,26 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
+    <hearderComponent />
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <cakeNew msg="xin chao cac anh em "/>
+    <footerComponent /> 
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import cakeNew from './components/cakeNew.vue'
+import hearderComponent from './components/headerComponent.vue'
+import footerComponent from './components/footerComponent.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    cakeNew,
+    hearderComponent,
+    footerComponent
   }
 }
 </script>
---
+++ hocvue1/src/components/cakeNew.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+        <h1>xin chao cac anh em </h1>
+        <p> Toi la tho cho toi xem tai</p>
+    </div>
+</template>
